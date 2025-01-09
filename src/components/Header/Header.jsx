1c12aa0befac8aadc316f2f8c21d3ab9bfbb9f13
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-01-09 15:30:44 +0700

    Changing the header width

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,18 @@ import phone from '../../icons/phone.svg';
 export default function Header() {
     return (
         <header>
-            <nav>
-                <a href="#">Главная</a>
-                <a href="#">О нас</a>
-                <a href="#">Контакты</a>
-                <a href="#">Поиск</a>
-            </nav>
+            <div className="header-container">
+                <nav>
+                    <a href="/">Главная</a>
+                    <a href="#">О нас</a>
+                    <a href="#">Контакты</a>
+                    <a href="#">Поиск</a>
+                </nav>
 
-            <div className="phone">
-                <img src={phone} alt="Телефон"/>
-                <a href="tel:+798788787">+7 (987) 887-87</a>
+                <div className="header-container__phone">
+                    <img src={phone} alt="Телефон"/>
+                    <a href="tel:+798788787">+7 (987) 887-87</a>
+                </div>
             </div>
         </header>
     )
